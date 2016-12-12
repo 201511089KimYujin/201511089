d4ecaf64f8c5a6249ac46edc61e18c823c8b4487
201511089KimYujin <ujin0327@naver.com>
2016-12-12 11:07:59 +0900

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,23 @@
+package com.sd.DiceGame;
+import com.sd.DiceGame.*;
+
+public class Player {
+	 private String name=null;
+	    int num=0;
+	    public Player(String _name) {
+	        name=_name;
+	    }
+	    public void play() {
+	        dice d=new dice();
+	        //num=d.roll();
+	        d.roll();
+	        num=d.getNum();
+	    }
+	    public String getName() {
+	        return this.name;
+	    }
+	    public int getNum() {
+	        return this.num;
+	    }
+
+}
